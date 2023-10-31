7e25b52a09ffc3144d43807fa34ab27e351553c6
Mark Anthony <touya1313@hotmail.com>
2023-10-31 15:54:48 -0600

    added plugins in correct spot

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,34 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-plugins: [
-  new HtmlWebpackPlugin({
-    template: 'src/index.html',
-    title: 'Webpack Plugin',
-  }),
-  new InjectManifest({
-    swSrc: 'src-sw.js',
-    swDest: 'service-worker.js',
-  }),
-  new WebpackPwaManifest({
-    name: 'Just Another Text Editor',
-    short_name: 'JATE',
-    description: 'Note taker offline and online',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: path.resolve('src/images/logo.png'),
-        sizes: [96, 128, 192, 256, 384, 512],
-      },
-    ],
-  }),
-  new InjectManifest({
-    swSrc: 'src-sw.js',
-    swDest: 'service-worker.js',
-  }),
-],
-
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -46,7 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: 'src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Note taker offline and online',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: 'src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
