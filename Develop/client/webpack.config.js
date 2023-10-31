d70d34ba1ca6f317151e0203338290bb8af1f6be
Mark Anthony <touya1313@hotmail.com>
2023-10-31 15:32:10 -0600

    starting webpack file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,13 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+plugins: [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    chunks: ['main'],
+  }),
+]
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
