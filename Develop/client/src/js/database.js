ed927b507e5508442e90a086c0bfdfebd50302f8
Mark Anthony <touya1313@hotmail.com>
2023-10-31 15:06:47 -0600

    added logic to getDb

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -24,6 +24,14 @@ export const putDb = async (content) => {
 // console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  return allContent;
+};
+
+console.error('getDb not implemented');
 
 initdb();
