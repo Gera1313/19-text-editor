3146fb01bc1e1747a5cd5c03be0dc9ddc5fa9203
Mark Anthony <touya1313@hotmail.com>
2024-05-30 21:44:23 -0600

    database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log('PUT to the database');
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -21,7 +22,6 @@ export const putDb = async (content) => {
   await tx.done;
   console.log(`Added content with ID: ${id}`);
 };
-// console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
