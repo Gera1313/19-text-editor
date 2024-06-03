eae0b267b99e0970e03d19d35b35091e15baacb0
Mark Anthony <touya1313@hotmail.com>
2024-06-03 16:02:15 -0600

    server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -12,4 +12,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
