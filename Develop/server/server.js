6828b23bb23d59eb5124674b38b7f0a61ad802c5
Mark Anthony <touya1313@hotmail.com>
2024-06-03 16:02:43 -0600

    formatted file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the client/dist directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // Fallback to serve index.html for any other route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
