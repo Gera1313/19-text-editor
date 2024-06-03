45cedce00820db8a4de28b96e438574779f72c72
Mark Anthony <touya1313@hotmail.com>
2024-06-03 16:01:22 -0600

    server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the client/dist directory
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-require('./routes/htmlRoutes')(app);
+// Fallback to serve index.html for any other route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
